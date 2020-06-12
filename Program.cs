081752907fa6185b6593f0e2d621d98e8ab49cb2
havidpradipta <61857246+havidpradipta@users.noreply.github.com>
2020-06-12 22:39:08 +0700

    in

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace OOP_Abstrac
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Implementasi imple = new Implementasi();
+			string nama = "Havid Pradipta Husada";
+			string almt = "Pati";
+            string Umur = "19";
+			string NIM = "2721";
+			string Jurusan = "Informatika";
+
+			imple.ShowKTP(nama, almt);
+			imple.ShowMHS(nama, NIM, Jurusan);
+
+			Console.ReadKey(true);
+		}
+	}
+}
